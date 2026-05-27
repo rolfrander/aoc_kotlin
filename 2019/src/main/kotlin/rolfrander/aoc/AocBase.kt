004b88b62dc911e80b255b5464f66f23e2d510f6
Rolf Rander Næss <rolfn-github@pvv.org>
2026-05-27 18:23:57 +0200

    merged changes to AocBase from main with more flexible formatting of results

--- 2019/src/main/kotlin/rolfrander/aoc/AocBase.kt
+++ 2019/src/main/kotlin/rolfrander/aoc/AocBase.kt
@@ -17,7 +17,7 @@ import org.apache.commons.logging.LogFactory
 
 abstract class AocBase(private val config: AocData, private val day: Int, private val testdata: String) {
 
-    val data: String by lazy { config.getData(day) }
+    val data = config.getData(day)
 
     var isTesting = false;
 
@@ -42,24 +42,31 @@ abstract class AocBase(private val config: AocData, private val day: Int, privat
         }
     }
 
+    /**
+     * To customize formatting override this function. It should return a block (lambda) that works on a kotlinx.html.BODY.
+     */
+    open fun formatResult(result: Any): BODY.() -> Unit {
+        return { p { + result.toString() } }
+    }
+
     fun compute(data: String, link: String, linkText: String): String {
-        var res1: String = ""
-        var res2: String = ""
+        var res1: Any = ""
+        var res2: Any = ""
         val time1 = measureTimeMillis {
-            res1 = part1(data).toString()
+            res1 = part1(data)
         }
         var time2 = measureTimeMillis { 
-            res2 = part2(data).toString()
+            res2 = part2(data)
         }
         val doc = createHTMLDocument().html {
             head { title { +"AOC 2019 Day ${day}" }}
             body {
                 h1 { +"AOC 2019 Day ${day}" }
                 h2 { +"Part 1" }
-                p { + res1 }
+                formatResult(res1)()
                 p { +"Time: ${time1} ms"}
                 h2 { +"Part 2"}
-                p { + res2 }
+                formatResult(res2)()
                 p { +"Time: ${time2} ms"}
                 p { a(link) { +linkText }}
             }
